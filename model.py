ff51cecabae1c14a9fa8f270582a282934ee922a
Ranitbag007 <133197492+Ranitbag007@users.noreply.github.com>
2023-11-10 17:02:59 +0530

    model.py

--- model.py
+++ model.py
@@ -1,24 +1,25 @@
+from transformers import PreTrainedModel
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-print(device)
-batch_size = 32
+from config import CustomAIConfig
+batch_size = 4
 block_size = 128
-max_iters = 200
+max_iters = 10
 learning_rate = 3e-4
 eval_iters = 100
-n_embd = 384
-n_head = 4
-n_layer = 4
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print(device)
+# n_embd = 384
+# n_head = 4
+# n_layer = 4
 dropout = 0.2
-from tokenizer import vocab_size
-
+# vocab_size=1000
 # model architecture
 class Head(nn.Module):
     """ one head of self-attention """
 
-    def __init__(self, head_size):
+    def __init__(self, n_embd, head_size):
         super().__init__()
         self.key = nn.Linear(n_embd, head_size, bias=False)
         self.query = nn.Linear(n_embd, head_size, bias=False)
@@ -28,34 +29,35 @@ class Head(nn.Module):
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
-
+        # input of size (batch, time-step, channels)
+        # output of size (batch, time-step, head size)
         B,T,C = x.shape
-        k = self.key(x)  
-        q = self.query(x) 
-    
-        wei = q @ k.transpose(-2,-1) * k.shape[-1]**-0.5 
-        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf')) 
-        wei = F.softmax(wei, dim=-1)
+        k = self.key(x)   # (B,T,hs)
+        q = self.query(x) # (B,T,hs)
+        # compute attention scores ("affinities")
+        wei = q @ k.transpose(-2,-1) * k.shape[-1]**-0.5 # (B, T, hs) @ (B, hs, T) -> (B, T, T)
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf')) # (B, T, T)
+        wei = F.softmax(wei, dim=-1) # (B, T, T)
         wei = self.dropout(wei)
-        v = self.value(x)
-        out = wei @ v
+        # perform the weighted aggregation of the values
+        v = self.value(x) # (B,T,hs)
+        out = wei @ v # (B, T, T) @ (B, T, hs) -> (B, T, hs)
         return out
 
 
 class MultiHeadAttention(nn.Module):
     """ multiple heads of self-attention in parallel """
 
-    def __init__(self, num_heads, head_size):
+    def __init__(self, num_heads, head_size, n_embd):
         super().__init__()
-        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.heads = nn.ModuleList([Head(n_embd,head_size) for _ in range(num_heads)])
         self.proj = nn.Linear(head_size * num_heads, n_embd)
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
-        out = torch.cat([h(x) for h in self.heads], dim=-1) 
+        out = torch.cat([h(x) for h in self.heads], dim=-1) # (B, T, F) -> (B, T, [h1, h1, h1, h1, h2, h2, h2, h2, h3, h3, h3, h3])
         out = self.dropout(self.proj(out))
         return out
-
 
 class FeedFoward(nn.Module):
     """ a simple linear layer followed by a non-linearity """
@@ -76,13 +78,14 @@ class Block(nn.Module):
     """ Transformer block: communication followed by computation """
 
     def __init__(self, n_embd, n_head):
-        
+        # n_embd: embedding dimension, n_head: the number of heads we'd like
         super().__init__()
         head_size = n_embd // n_head
-        self.sa = MultiHeadAttention(n_head, head_size)
+        self.sa = MultiHeadAttention(n_head, head_size,n_embd)
         self.ffwd = FeedFoward(n_embd)
         self.ln1 = nn.LayerNorm(n_embd)
         self.ln2 = nn.LayerNorm(n_embd)
+
 
     def forward(self, x):
         y = self.sa(x)
@@ -91,17 +94,19 @@ class Block(nn.Module):
         x = self.ln2(x + y)
         return x
 
-class Recag(nn.Module):
-    def __init__(self, vocab_size):
-        super().__init__()
-        self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
-        self.position_embedding_table = nn.Embedding(block_size, n_embd)
-        self.blocks = nn.Sequential(*[Block(n_embd, n_head=n_head) for _ in range(n_layer)])
-        self.ln_f = nn.LayerNorm(n_embd) # final layer norm
-        self.lm_head = nn.Linear(n_embd, vocab_size)
-
+class CustomAI(PreTrainedModel):
+    config_class = CustomAIConfig
+    def __init__(self, config):
+        super().__init__(config)
+        self.token_embedding_table = nn.Embedding(config.vocab_size, config.n_embd)
+        self.position_embedding_table = nn.Embedding(block_size, config.n_embd)
+        self.blocks = nn.Sequential(*[Block(config.n_embd, n_head=config.n_head) for _ in range(config.n_layer)])
+        self.ln_f = nn.LayerNorm(config.n_embd)  # final layer norm
+        self.lm_head = nn.Linear(config.n_embd, config.vocab_size)
 
         self.apply(self._init_weights)
+
+
 
     def _init_weights(self, module):
         if isinstance(module, nn.Linear):
@@ -149,5 +154,15 @@ class Recag(nn.Module):
             # append sampled index to the running sequence
             index = torch.cat((index, index_next), dim=1) # (B, T+1)
         return index
+config = CustomAIConfig(
+        vocab_size=1000,
+        n_embd=384,
+        n_head=4,
+        n_layer=4,
+        dropout=0.2,
+    # any other parameters you want to set
+)
+model = CustomAI(config)
+# model.load_state_dict(torch.load('/content/BharatAI.pth'))
 
-model = Recag(vocab_size)
+
